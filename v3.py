9782109c3981c84db8fd3511ec1f2780cc11473e
staticor <staticor0828@gmail.com>
2015-04-07 22:37:31 +0800

    v3.  df.to_csv.

--- v3.py
+++ v3.py
@@ -6,20 +6,18 @@ Created on April , 2015
 import os
 import time
 from datetime import datetime
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-from pandas import Series, DataFrame
+from pandas import DataFrame
 import sys
 
 if sys.platform.startswith('darwin'):
-    path = '/Users/staticor/Downloads/intraQuarter/_KeyStats/' # mac
+    path = '/Users/staticor/Downloads/intraQuarter/_KeyStats/'  # mac
 else:
-    path = 'D:/intraQuarter/_KeyStats'# Windows
+    path = 'D:/intraQuarter/_KeyStats'  # Windows
+
 
 def Key_Stats(gather="Total Debt/Equity (mrq)"):
-    stock_list = [ x[0] for x in os.walk(path)]
-    #print(stock_list)
+    stock_list = [x[0] for x in os.walk(path)]
+    df = DataFrame(columns=['Date', 'Unix', 'Ticker', 'DE Ratio'])
     for each_dir in stock_list[1:]:
         each_file = os.listdir(each_dir)
         ticker = each_dir.split('/')[-1]
@@ -27,19 +25,16 @@ def Key_Stats(gather="Total Debt/Equity (mrq)"):
             for file in each_file:
                 date_stamp = datetime.strptime(file, '%Y%m%d%H%M%S.html')
                 unix_time = time.mktime(date_stamp.timetuple())
-                #print(date_stamp, unix_time)
                 full_file_path = each_dir + '/' + file
                 print(full_file_path)
                 source = open(full_file_path, 'r').read()
-                #print(source)
                 try:
-                    value = float(source.split(gather+':</td><td class="yfnc_tabledata1">')[1].split('</td>')[0])
-                    print(ticker+':' ,  value)
+                    value = float(source.split(gather + ':</td><td class="yfnc_tabledata1">')[1].split('</td>')[0])
+                    df = df.append({'Date': date_stamp, 'Unix': unix_time, 'Ticker': ticker, 'DE Ratio': value}, ignore_index=True)
                 except Exception as e:
                     pass
-                time.sleep(3)
-
-
+                #time.sleep(3)
+    save =gather.replace(' ', '').replace('(', '').replace(')', '').replace('/', '') + ('.csv')
+    df.to_csv(save)
 
 Key_Stats()
-
