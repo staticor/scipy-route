f163596d927e8cd6b55486352423cc32adf91616
staticor <staticor0828@gmail.com>
2015-04-09 17:46:16 +0800

    add # 2. svm instance for datasets(digits).

--- scipy-example-digits-svm.py
+++ scipy-example-digits-svm.py
@@ -1,16 +1,27 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Apr 09 09:32:12 2015
-
+'life is short, python is awesome!'
 @author: STEVE
 """
-#
-#import matplotlib.pyplot as plt 
-#from scipy import datasets
-#
-#digits = datasets.l
-#
-#plt.show()
 
-print('hello world!')
-print('life is short, python is awesome!')
+import matplotlib.pyplot as plt 
+from sklearn import datasets
+from sklearn import svm
+
+digits = datasets.load_digits()
+
+# check the source data's volume
+print('We have total about {count} rows of data'.format(count=len(digits.data)))
+
+
+clf = svm.SVC(gamma=0.001, C=100)
+x, y = digits.data[:-1], digits.target[:-1]
+clf.fit(x, y)
+
+print('Predictions:', clf.predict(digits.data[-3]))
+
+# show the real digits picture
+plt.imshow(digits.images[-3], cmap=plt.cm.gray_r, interpolation='nearest')
+
+plt.show()
