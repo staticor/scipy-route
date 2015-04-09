f58b181c54cea6b8e0a26dd7a73668df5efc1269
staticor <staticor0828@gmail.com>
2015-04-09 14:47:48 +0800

    scipy , update.

--- v4.py
+++ v4.py
@@ -8,7 +8,7 @@ import time
 from datetime import datetime
 from pandas import DataFrame
 import sys
-
+import matplotlib.pyplot as plt
 if sys.platform.startswith('darwin'):
     path = '/Users/staticor/Downloads/intraQuarter/_KeyStats/'  # mac
 else:
@@ -19,7 +19,7 @@ def Key_Stats(gather="Total Debt/Equity (mrq)"):
     stock_list = [x[0] for x in os.walk(path)]
     df = DataFrame(columns=['Date', 'Unix', 'Ticker', 'DE Ratio'])
     sp500_df = DataFrame.from_csv('YAHOO-INDEX_GSPC.csv')
-    for each_dir in stock_list[1:3]:
+    for each_dir in stock_list[1:30]:
         each_file = os.listdir(each_dir)
         ticker = each_dir.split('/')[-1]
         if len(each_file) > 0:
@@ -27,7 +27,7 @@ def Key_Stats(gather="Total Debt/Equity (mrq)"):
                 date_stamp = datetime.strptime(file, '%Y%m%d%H%M%S.html')
                 unix_time = time.mktime(date_stamp.timetuple())
                 full_file_path = each_dir + '/' + file
-                print(full_file_path)
+                #print(full_file_path)
                 source = open(full_file_path, 'r').read()
                 try:
                     value = float(source.split(gather + ':</td><td class="yfnc_tabledata1">')[1].split('</td>')[0])
@@ -55,5 +55,7 @@ def Key_Stats(gather="Total Debt/Equity (mrq)"):
                 #time.sleep(3)
     save =gather.replace(' ', '').replace('(', '').replace(')', '').replace('/', '') + ('.csv')
     df.to_csv(save)
-
+    
+    df[['Price', 'SP500']].plot()
+    plt.show()
 Key_Stats()
