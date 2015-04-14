ff640c9ada9ce98d6fd5d94d26bd59868e50dd28
staticor <staticor0828@gmail.com>
2015-04-14 11:49:34 +0800

    #

--- sklearn-dt1.py
+++ sklearn-dt1.py
@@ -42,4 +42,48 @@ from sklearn.externals.six import StringIO
 with open('iris.dot', 'w') as f:
     f = tree.export_graphviz(clf, out_file=f)
 import os
-# create a pdf = file
+
+
+
+
+
+
+
+
+
+# sklearn.tree . DecisionTreeClassifier
+# class sklearn.tree.DecisionTreeClassifier(criterion='gini', splitter='best', max_depth=None, min_samples_split=2, min_samples_leaf=1, min_weight_fraction_leaf=0.0, max_features=None, random_state=None, max_leaf_nodes=None, class_weight=None)[source]¶
+
+
+## Parameters
+
+### criterion
+
+# source code: https://github.com/scikit-learn/scikit-learn/blob/2b3fabc/sklearn/tree/tree.py#L396
+
+
+'''
+tree 能使用的 几种方法
+
+__all__ = ["DecisionTreeClassifier",
+            "DecisionTreeRegressor",
+            "ExtraTreeClassifier",
+            "ExtraTreeRegressor"]
+
+
+'''
+
+from sklearn.datasets import load_iris
+from sklearn.cross_validation import cross_val_score
+from sklearn.tree import DecisionTreeClassifier
+
+clf = DecisionTreeClassifier(random_state=0)
+iris = load_iris()
+r = cross_val_score(clf, iris.data, iris.target, cv=10)
+print r
+''' output
+>>> cross_val_score(clf, iris.data, iris.target, cv=10)
+
+array([ 1.        ,  0.93333333,  1.        ,  0.93333333,  0.93333333,
+        0.86666667,  0.93333333,  1.        ,  1.        ,  1.        ])
+'''
