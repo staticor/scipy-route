81d6d6fc59e389b5595589ddacf2689bc0b5e642
staticor <staticor0828@gmail.com>
2015-04-29 18:29:55 +0800

    ols simple. done, add a isotonic frame (not learn)

--- linear_model/ols/ols_example.py
+++ linear_model/ols/ols_example.py
@@ -1,24 +1,48 @@
 # -*- coding: utf-8 -*-
 '''
 Created on April , 2015
+link: http://scikit-learn.org/stable/auto_examples/linear_model/plot_ols.html
 @author: stevey
 '''
 
 import numpy as np
-import pandas as pd
 from matplotlib import style
 import matplotlib.pyplot as plt
-from pandas import Series, DataFrame
-import os, sys
-from sklearn import datasets
+
+from sklearn import datasets, linear_model
 style.use('ggplot')
 
 
 # load data
 diabetes = datasets.load_diabetes()
 # select feature to analysis
-diabetes_X = diabetes.data[:, np.newaxis]
+diabetes_X = np.array(diabetes.data[:, np.newaxis])
+diabetes_X = diabetes_X[:, :, 2]
 dx_train = diabetes_X[:-20]
 dx_test = diabetes_X[-20:]
+dy_train = diabetes.target[:-20]
+dy_test = diabetes.target[-20:]
+
+regr = linear_model.LinearRegression()
+regr.fit(dx_train, dy_train)
+
+# output
+intercept = regr.intercept_
+coef = regr.coef_
+
+# estimator: regression coefficient
+print(intercept, coef)
+
+
+fig, ax0 = plt.subplots(nrows=1)
+fig = plt.figure(figsize=(10, 8))
+
+plt.rc('lines', linewidth=4)
+plt.scatter(dx_test, dy_test, color='red')
+plt.plot(dx_test, regr.predict(dx_test), color='blue')
+plt.title('A simple OLS model')
+plt.xlabel('input x')
+plt.ylabel('predict y')
+plt.show()
 
 
